665fde987edb365207738ac723e1903a35ea9463
qinmengmei <qinmengmei.sx@fosun.com>
2017-06-08 12:51:19 +0800

    responseMessage

--- src/main/java/com/qmm/controller/UserController.java
+++ src/main/java/com/qmm/controller/UserController.java
@@ -1,29 +1,42 @@
 package com.qmm.controller;
 
+import com.qmm.common.ResponseMessage;
 import com.qmm.entity.User;
 import com.qmm.service.UserService;
+import com.qmm.vo.MyUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Administrator on 2017/6/7.
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/showUser")
+    @RequestMapping(value = "/showUser",method = RequestMethod.GET)
     public String toIndex(HttpServletRequest request, Model model){
         int userId = Integer.parseInt(request.getParameter("id"));
         User user = this.userService.getUserById(userId);
         model.addAttribute("user", user);
         return "showUser";
+    }
+
+    @RequestMapping(value = "/show",method = RequestMethod.GET)
+    public ResponseMessage showUser(Integer id){
+        User user = this.userService.getUserById(id);
+        MyUser myUser = new MyUser();
+        myUser.setId(user.getId());
+        myUser.setName(user.getUserName());
+        myUser.setAge(user.getAge());
+        return new ResponseMessage().success().packageData("list",myUser);
     }
 }
