24b1cbedfd301c6ddceddfe91147ff3e3fe938d5
qinmengmei <qinmengmei.sx@fosun.com>
2017-06-08 17:14:39 +0800

    前后端分离

--- src/main/java/com/qmm/controller/UserController.java
+++ src/main/java/com/qmm/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +26,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    //前后端不分离
     @RequestMapping(value = "/showUser",method = RequestMethod.GET)
     public String toIndex(HttpServletRequest request, Model model){
         int userId = Integer.parseInt(request.getParameter("id"));
@@ -34,7 +36,9 @@ public class UserController {
         return "showUser";
     }
 
+    //前后端分离
     @RequestMapping(value = "/show",method = RequestMethod.GET)
+    @ResponseBody
     public ResponseMessage showUser(Integer id){
         User user = this.userService.getUserById(id);
         MyUser myUser = new MyUser();
